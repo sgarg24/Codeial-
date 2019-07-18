5cccfa70e0f2407d34884575f6e62812934b3ec8
Gitesh <wadhwagitesh2@gmail.com>
2019-07-18 22:37:47 +0530

    Adding comments to the database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,18 +1,9 @@
 const Post = require('../models/post');
 
 module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
     // populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
+        console.log(posts.user);
         return res.render('home', {
             title: "Codeial | Home",
             posts:  posts
