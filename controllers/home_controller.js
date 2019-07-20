b5c75e09624a75e43399311271e6d79063c8a7d9
Gitesh <wadhwagitesh2@gmail.com>
2019-07-20 22:28:20 +0530

    Deleting a post (Authorized)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,16 +1,6 @@
 const Post = require('../models/post');
 
 module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
     // populate the user of each post
     Post.find({})
     .populate('user')
@@ -21,11 +11,12 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
+        console.log(posts,'----------------');
         return res.render('home', {
             title: "Codeial | Home",
             posts:  posts
         });
-    })
+    });
 
 }
 
