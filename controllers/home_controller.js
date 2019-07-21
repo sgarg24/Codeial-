a4bc8e37f4f6a386eb8bb54b0736ec0d811c28d9
Gitesh <wadhwagitesh2@gmail.com>
2019-07-21 16:34:34 +0530

    users which are logged in (user profile links)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const User=require('../models/user');
 module.exports.home = function(req, res){
     // populate the user of each post
     Post.find({})
@@ -11,11 +11,17 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        console.log(posts,'----------------');
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+
+        User.find({},function(err,users){
+           
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users:users
+            });
+
         });
+       
     });
 
 }
