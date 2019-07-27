494ab4da53b8c11d1d815d6e27ce0d5fbf5ab018
Gitesh <wadhwagitesh2@gmail.com>
2019-07-27 23:45:17 +0530

    Saving file from params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,23 +1,54 @@
 const User = require('../models/user');
 
-
+// let's keep it same as before
 module.exports.profile = function(req, res){
-    User.findById(req.params.id,function(err,user){
+    User.findById(req.params.id, function(err, user){
         return res.render('user_profile', {
             title: 'User Profile',
-            profile_user:user
+            profile_user: user
         });
     });
-    
+
 }
 
-module.exports.update=function(req,res){
-    if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
-              return res.redirect('back');
-        });
-    }else{
-        return res.status(401).send('unauthorized');
+
+module.exports.update = async function(req, res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unauthorized');
+    // }
+    if(req.user.id == req.params.id)
+    {
+          try{
+              let user=await User.findById(req.params.id);
+               User.uploadedAvatar(req,res,function(err){
+                 if(err){
+                     console.log('***********multer error***',err);
+                    }
+                // console.log(req.file);
+                user.name=req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                    }      
+                user.save();
+                return res.redirect('back');
+               });
+          }catch(err){
+            req.flash('error', err);
+            return res.redirect('back');
+          }
+    }
+    else{
+         req.flash('error', 'Unauthorized!');
+         return res.status(401).send('Unauthorized');
     }
 }
 
@@ -49,19 +80,21 @@ module.exports.signIn = function(req, res){
 // get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
 
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+        if(err){req.flash('error', err); return}
 
         if (!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
+                if(err){req.flash('error', err); return}
 
                 return res.redirect('/users/sign-in');
             })
         }else{
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
 
@@ -71,12 +104,14 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    req.flash('success','Logged in Successfully');
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
-    req.flash('success','You have logged out ');
+    req.flash('success', 'You have logged out!');
+
+
     return res.redirect('/');
 }
--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const multer=require('multer');
-const path=require('path');
-const  AVATAR_PATH=path.join('/uploads/users/avatars');
+
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -17,9 +18,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    avatar:{
-        type:String,
-
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true
@@ -28,12 +28,18 @@ const userSchema = new mongoose.Schema({
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now());
     }
-  })
+  });
+
+
+// static
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
 
 
 const User = mongoose.model('User', userSchema);
