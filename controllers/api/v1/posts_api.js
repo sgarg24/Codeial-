9e825f1acb7f3b1be1491dd3ed23212097eacc74
Gitesh <wadhwagitesh2@gmail.com>
2019-08-03 09:24:02 +0530

    passport-jwt installed

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,5 +1,5 @@
 const Post = require('../../../models/post');
-
+const Comment=require('../../../models/comment');
 
 module.exports.index=async function(req,res){
 
@@ -19,4 +19,37 @@ module.exports.index=async function(req,res){
         message:"List of posts",
         posts:posts
     })
+}
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+       // if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            
+
+        
+
+            return res.json(200,{
+                message:"Post and associated comments deleted successfully"
+            });
+    // }else{
+    //         req.flash('error', 'You cannot delete this post!');
+    //         return res.redirect('back');
+    //     }
+
+    }catch(err){
+        console.log('*********',err);
+        return res.json(500,{
+            message:"Internal Server Error"
+        });
+    }
+    
 }
